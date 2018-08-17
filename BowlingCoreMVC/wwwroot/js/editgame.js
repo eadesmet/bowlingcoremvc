9585544998fa9d50fd48cbeec8e5c91951c8050e
edesmet work-pc <edesmet work-pc@localhost>
2018-08-17 14:51:30 -0400

    missedallclick button bug fix

--- BowlingCoreMVC/wwwroot/js/editgame.js
+++ BowlingCoreMVC/wwwroot/js/editgame.js
@@ -581,14 +581,17 @@ $(document).ready(function ()
 
 function MissedAllClick(GameID)
 {
-    MissedAllClick.toggle = !MissedAllClick.toggle;
-    if (MissedAllClick.toggle)
+    //MissedAllClick.toggle = !MissedAllClick.toggle;
+    //if (MissedAllClick.toggle)
+    if ($('#' + GameID + '-btnMissedAll').val() == "true")
     {
         SetMissedPinsChecked(GameID, MISSED_ALL);
+        $('#' + GameID + '-btnMissedAll').val(false);
     }
     else
     {
         SetMissedPinsChecked(GameID, MISSED_0);
+        $('#' + GameID + '-btnMissedAll').val(true);
     }
 }
 
